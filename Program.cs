c294e4cdb9fdf06da04519c2ebb8c7df1835eb2c
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-03-20 16:32:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using CA230320;
+using System.Text;
+
+List<Versenyzo> versenyzok = new();
+
+using StreamReader sr = new("..\\..\\..\\src\\ub2017egyeni.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    string sor = sr.ReadLine();
+    Versenyzo ujVersenyzo = new(sor);
+    versenyzok.Add(ujVersenyzo);
+}
+
+int indulokSzam = versenyzok.Count;
+Console.WriteLine($"3. feladat: egyéni indulók száma {indulokSzam} fő");
+
---
+++ Versenyzo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230320
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int RajtSzam { get; set; }
+        public string Kategoria { get; set; }
+        public string Idoeredmeny { get; set; }
+        public int TavSzazalek { get; set; }
+
+        public Versenyzo(string fileEgySora)
+        {
+            string[] aFileSoranakDarabjai = fileEgySora.Split(';');
+            this.Nev = aFileSoranakDarabjai[0];
+            this.RajtSzam = int.Parse(aFileSoranakDarabjai[1]);
+            this.Kategoria = aFileSoranakDarabjai[2];
+            this.Idoeredmeny = aFileSoranakDarabjai[3];
+            this.TavSzazalek = int.Parse(aFileSoranakDarabjai[4]);
+        }
+    }
+}
